79ed7a0ee333133f4a897ea21603bd6eeeed6390
BB223 <79644542+BB223@users.noreply.github.com>
2025-05-22 17:42:41 +0200

    update vars

--- .config/sh/env.sh
+++ .config/sh/env.sh
@@ -6,19 +6,24 @@ export PASSWORD_STORE_ENABLE_EXTENSIONS=true
 # XDG dir fixes
 # config
 export ANDROID_SDK_HOME="$XDG_CONFIG_HOME"
-export ANDROID_USER_HOME="$XDG_CONFIG_HOME/.android"
-export DOCKER_CONFIG="$XDG_CONFIG_HOME/docker"
-export BUNDLE_USER_CONFIG="$XDG_CONFIG_HOME/bundle/config"
-export FFMPEG_DATADIR="$XDG_CONFIG_HOME/ffmpeg"
-export KUBECONFIG="$XDG_CONFIG_HOME/kube/config"
+export ANDROID_USER_HOME="$XDG_CONFIG_HOME/android"
+export DOCKER_CONFIG="$XDG_CONFIG_HOME"/docker
+export BUNDLE_USER_CONFIG=$XDG_CONFIG_HOME/bundle/config
+export FFMPEG_DATADIR="$XDG_CONFIG_HOME"/ffmpeg
+export KUBECONFIG="$XDG_CONFIG_HOME/kube"
 # data
-export NVM_DIR="$XDG_DATA_HOME/nvm"
-export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
-export CARGO_HOME="$XDG_DATA_HOME/cargo"
-export GOPATH="$XDG_DATA_HOME/go"
-export BUNDLE_USER_PLUGIN="$XDG_DATA_HOME/bundle"
-export GRADLE_USER_HOME="$XDG_DATA_HOME/gradle"
+export NVM_DIR="$XDG_DATA_HOME"/nvm
+export RUSTUP_HOME="$XDG_DATA_HOME"/rustup
+export CARGO_HOME="$XDG_DATA_HOME"/cargo
+export GOPATH="$XDG_DATA_HOME"/go
+export BUNDLE_USER_PLUGIN=$XDG_DATA_HOME/bundle
+export GRADLE_USER_HOME="$XDG_DATA_HOME"/gradle
+if [[ -d "/mnt/d" ]]; then
+    export IPFS_PATH="/mnt/d/ipfs"
+else
+    export IPFS_PATH="$XDG_DATA_HOME"/ipfs
+fi
 # cache
-export GOMODCACHE="$XDG_CACHE_HOME/go/mod"
-export BUNDLE_USER_CACHE="$XDG_CACHE_HOME/bundle"
+export GOMODCACHE="$XDG_CACHE_HOME"/go/mod
+export BUNDLE_USER_CACHE=$XDG_CACHE_HOME/bundle
 export KUBECACHEDIR="$XDG_CACHE_HOME/kube"
