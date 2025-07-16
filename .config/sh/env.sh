bac56fea7031d03350b51d31d6ba949e2103f88d
BB223 <79644542+BB223@users.noreply.github.com>
2025-07-16 12:57:50 +0200

    improvements

--- .config/sh/env.sh
+++ .config/sh/env.sh
@@ -3,6 +3,7 @@ export LC_COLLATE="C.UTF-8"
 export GPG_TTY=$(tty)
 export PASSWORD_STORE_ENABLE_EXTENSIONS=true
 export QT_QPA_PLATFORMTHEME=qt5ct:qt6ct
+export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/ssh-agent.socket
 
 # XDG dir fixes
 # config
@@ -11,7 +12,7 @@ export ANDROID_USER_HOME="$XDG_CONFIG_HOME/android"
 export DOCKER_CONFIG="$XDG_CONFIG_HOME"/docker
 export BUNDLE_USER_CONFIG=$XDG_CONFIG_HOME/bundle/config
 export FFMPEG_DATADIR="$XDG_CONFIG_HOME"/ffmpeg
-export KUBECONFIG="$XDG_CONFIG_HOME/kube"
+export KUBECONFIG="$XDG_CONFIG_HOME/kube/config"
 # data
 export NVM_DIR="$XDG_DATA_HOME"/nvm
 export RUSTUP_HOME="$XDG_DATA_HOME"/rustup
