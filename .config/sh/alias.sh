6df0ce40e2ccfc2804f86f3a7bc6b65cd5a0ee0b
BB223 <79644542+BB223@users.noreply.github.com>
2025-10-29 08:23:09 +0100

    updates

--- .config/sh/alias.sh
+++ .config/sh/alias.sh
@@ -1,11 +1,9 @@
-alias ls='ls --color=auto'
+alias ls='ls --color=auto -F'
 alias grep='grep --color=auto'
 alias vim=nvim
 dotfiles() {
     /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME "$@"
 }
-# alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-alias ll='ls -alF'
+alias ll='ls -al'
 alias la='ls -A'
-alias l='ls -CF'
 alias grep='rg'
